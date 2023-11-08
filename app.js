a0b298801767f6cc801c66beec2fc255436b42ab
Sir Cliff <dev.clfm5t12@gmail.com>
2023-11-08 08:47:56 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// Function to create and append list items to the ul element using the template
+function createItemList() {
+    const itemList = document.getElementById("itemList");
+    const template = document.getElementById("item-template");
+
+    // Fetch the JSON data from the external file
+    fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            // Loop through the JSON data
+            data.forEach(function(item) {
+                // Clone the template content and populate it with data
+                const listItem = document.importNode(template.content, true);
+                listItem.querySelector('strong').textContent = item.name;
+                listItem.querySelector('span').textContent = item.description;
+                itemList.appendChild(listItem);
+            });
+        })
+        .catch(error => console.error('Error fetching data:', error));
+}
+
+// Call the function to create the item list
+createItemList();
